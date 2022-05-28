3d3001ae3e3f983544d4a49eee0921427465d6e3
bvashokkumar <bvashok123@gmail.com>
2022-05-28 21:30:26 +0530

    sub array sum count

---
+++ subarray-sum-count.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, x;
+	cin>>n>>x;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	int sum=0;
+	int ans=0;
+	map<int, int> m;
+	m[0]=1;
+	for(auto e:a){
+		sum+=e;
+		ans+=m[sum-x];
+		m[sum]++;
+	}
+	cout<<ans<<"\n";
+	return 0;
+}
